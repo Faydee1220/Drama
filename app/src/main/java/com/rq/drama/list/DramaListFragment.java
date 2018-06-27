cfd0150abdb69b1777de6c59cad466597d35ba31
Faydee <faydee1220@gmail.com>
2018-06-27 14:58:55 +0800

    add Divider

--- app/src/main/java/com/rq/drama/list/DramaListFragment.java
+++ app/src/main/java/com/rq/drama/list/DramaListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -54,6 +55,9 @@ public class DramaListFragment extends Fragment implements DramaListContract.Vie
 
     mAdapter = new DramaListAdapter(new ArrayList<>(), mPresenter);
     recyclerView.setAdapter(mAdapter);
+
+    recyclerView.addItemDecoration(new DividerItemDecoration(
+        MyApplication.getAppContext(), DividerItemDecoration.VERTICAL));
 
     mPresenter.start();
   }
